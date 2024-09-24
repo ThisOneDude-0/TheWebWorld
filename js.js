7ac789ade9960ee8f606f1e81613170bc7b32570
ThisOneDude-0 <this1dude.0@gmail.com>
2024-09-24 06:39:21 -0600

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+function inf() {
+  const inp = document.getElementById("in1")
+  document.getElementById("p1").innerHTML = "Hello, "+inp.value;
+  setTimeout(0, 3000);
+  window.location.href = "htt";
+}
